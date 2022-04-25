3dcab10f6f427e957aa71603225490241678ea60
yuanzhihai <396751927@qq.com>
2022-04-25 14:45:25 +0800

    修复助手函数

--- src/helper.php
+++ src/helper.php
@@ -9,6 +9,6 @@ if (!function_exists('event')) {
      */
     function event($event, array $payload = [], bool $halt = false)
     {
-        \yzh52521\event\Event::dispatch($event, $payload = [], $halt = false);
+        \yzh52521\event\Event::dispatch($event, $payload, $halt);
     }
 }
